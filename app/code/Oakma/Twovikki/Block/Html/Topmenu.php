3f2c5e7cbc6ad08c5b4922009f5e5b0e1c5afc54
Evgeniy Dubskiy <www.developer.local@gmail.com>
2018-12-10 22:38:47 +0200

    temp commit

---
+++ app/code/Oakma/Twovikki/Block/Html/Topmenu.php
@@ -0,0 +1,97 @@
+<?php
+namespace Oakma\Twovikki\Block\Html;
+
+use Magento\Theme\Block\Html\Topmenu as TopmenuCore;
+
+/**
+ * Html page top menu block
+ *
+ * @api
+ * @since 100.0.2
+ */
+class Topmenu extends TopmenuCore
+{
+
+    /**
+     * Recursively generates top menu html from data that is specified in $menuTree
+     *
+     * @param \Magento\Framework\Data\Tree\Node $menuTree
+     * @param string $childrenWrapClass
+     * @param int $limit
+     * @param array $colBrakes
+     * @return string
+     *
+     * @SuppressWarnings(PHPMD.CyclomaticComplexity)
+     * @SuppressWarnings(PHPMD.NPathComplexity)
+     */
+    protected function _getHtml(
+        \Magento\Framework\Data\Tree\Node $menuTree,
+        $childrenWrapClass,
+        $limit,
+        $colBrakes = []
+    ) {
+        $html = '';
+
+        $children = $menuTree->getChildren();
+        $parentLevel = $menuTree->getLevel();
+        $childLevel = $parentLevel === null ? 0 : $parentLevel + 1;
+
+        $counter = 1;
+        $itemPosition = 1;
+        $childrenCount = $children->count();
+
+        $parentPositionClass = $menuTree->getPositionClass();
+        $itemPositionClassPrefix = $parentPositionClass ? $parentPositionClass . '-' : 'nav-';
+
+        /** @var \Magento\Framework\Data\Tree\Node $child */
+        foreach ($children as $child) {
+            if ($childLevel === 0 && $child->getData('is_parent_active') === false) {
+                continue;
+            }
+            $child->setLevel($childLevel);
+            $child->setIsFirst($counter == 1);
+            $child->setIsLast($counter == $childrenCount);
+            $child->setPositionClass($itemPositionClassPrefix . $counter);
+
+            $outermostClassCode = '';
+            $outermostClass = $menuTree->getOutermostClass();
+
+            if ($childLevel == 0 && $outermostClass) {
+                $outermostClassCode = ' class="' . $outermostClass . '" ';
+                $currentClass = $child->getClass();
+
+                if (empty($currentClass)) {
+                    $child->setClass($outermostClass);
+                } else {
+                    $child->setClass($currentClass . ' ' . $outermostClass);
+                }
+            }
+
+            $html .= '<li ' . $this->_getRenderedMenuItemAttributes($child) . '>';
+            $html .= '<a href="' . $child->getUrl() . '" ' . $outermostClassCode . '>' . $this->escapeHtml(
+                    $child->getName()
+                ) . '</a></li>';
+            $itemPosition++;
+            $counter++;
+        }
+
+        return $html;
+    }
+
+    /**
+     * Returns array of menu item's classes
+     *
+     * @param \Magento\Framework\Data\Tree\Node $item
+     * @return array
+     */
+    protected function _getMenuItemClasses(\Magento\Framework\Data\Tree\Node $item)
+    {
+        $classes = [];
+
+        if ($item->getIsActive()) {
+            $classes[] = 'uk-active';
+        }
+
+        return $classes;
+    }
+}
