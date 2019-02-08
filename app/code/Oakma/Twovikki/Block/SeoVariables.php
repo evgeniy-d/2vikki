4caf4449f717329b225f2b6ff4be787c670affd3
Evgeniy Dubskiy <www.developer.local@gmail.com>
2019-02-08 15:14:17 +0200

    commit temp

---
+++ app/code/Oakma/Twovikki/Block/SeoVariables.php
@@ -0,0 +1,15 @@
+<?php
+namespace Oakma\Twovikki\Block;
+
+use Magento\Framework\View\Element\Template;
+use Magento\Catalog\Block\Category\View as CategoryView;
+/**
+ * Set seo variables in places where content block has been removed
+ *
+ * @api
+ * @since 1.0.1
+ */
+class SeoVariables extends CategoryView
+{
+
+}
---
+++ app/code/Oakma/Twovikki/Plugin/View/Page/Title.php
@@ -0,0 +1,14 @@
+<?php
+namespace Oakma\Twovikki\Plugin\View\Page;
+
+/**
+ * Interceptor for Title class
+ *
+ * @api
+ * @since 1.0.1
+ */
+class Title
+{
+
+
+}
